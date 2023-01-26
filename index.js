1990177c3c2a4e8ae695c82e13a3c07de022b848
mix.piyadanai@gmail.com <0985690625Zebra>
2023-01-26 19:48:03 +0700

    747

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/user', (req, res) => {
+    res.send('show user')
+})
 app.listen(port, () => {
     console.log('server started')
 });
